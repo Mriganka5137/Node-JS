a41bb1cd47e51bc59becf8f56a265e6e28a53d93
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-21 14:47:05 +0530

    Connection string

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-
-const connectionString = `mongodb+srv://mrigankagogoi5137:Z6pgGoHVLP3bxwNT@cluster0.r5ijqes.mongodb.net/03-TASK_MANAGER?retryWrites=true&w=majority`;
-
 const connectDB = (url) => {
   return mongoose.connect(connectionString);
 };
