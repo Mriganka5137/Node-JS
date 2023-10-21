bb1c9f5270ea7dec6e29d53088e385cbe0b563f5
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-21 16:42:34 +0530

    All Mongoose methods

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,22 +1,78 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("Get All Tasks");
-};
-const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
-};
-const getTask = (req, res) => {
-  res.send("Get Task");
-};
-const updateTask = (req, res) => {
-  res.send("Update Task");
-};
-const deleteTask = (req, res) => {
-  res.send("Delete Task");
+// Get all tasks from DB
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({}); //----> find all the tasks
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
+// Create a task
+const createTask = async (req, res) => {
+  // Try Catch Block to handle errors
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+// Get a single task
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `Task not available for id ${taskId}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+// Delete a Task
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No Task with id ${taskId}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+// Update a Task
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No Task with id ${taskId}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 module.exports = {
   getAllTasks,
   createTask,
