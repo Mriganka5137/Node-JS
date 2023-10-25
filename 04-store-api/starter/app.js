4f9bc1f99e3fb890698abdb98a43e9fd459a9190
Mriganka5137 <gogoimriganka09@gmail.com>
2023-10-25 19:20:20 +0530

    Initial Setup for the STORE API

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 // async errors
-
+require("express-async-errors"); // -------> This is a middleware that will catch all the errors that are not catched by the try catch block
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
-
+const productsRouter = require("./routes/products");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 // Router
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
